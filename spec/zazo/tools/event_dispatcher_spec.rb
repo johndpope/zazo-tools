9b3b778f89d263bf502449a383171f51a9e11597
Ivan Kornilov <vano468@gmail.com>
2016-04-05 22:23:32 +0300

    Refactor specs

--- spec/zazo/tools/event_dispatcher_spec.rb
+++ spec/zazo/tools/event_dispatcher_spec.rb
@@ -4,25 +4,22 @@ require 'zazo/tools/event_dispatcher'
 RSpec.describe Zazo::Tools::EventDispatcher do
   describe '.emit', event_dispatcher: true do
     let(:name) { %w(event_dispatcher test) }
-    let(:params) do
-      { initiator: 'user',
-        initiator_id: '1' }
-    end
+    let(:params) { { initiator: 'user', initiator_id: '1' } }
     subject { described_class.emit(name, params) }
+
     around do |example|
-      VCR.use_cassette('sqs_send_message', erb: {
-                         queue_url: described_class.queue_url,
-                         region: described_class.sqs_client.config.region,
-                         access_key: described_class.sqs_client.config.credentials.access_key_id }) do
-        example.run
-      end
+      erb = { queue_url: described_class.queue_url,
+              region: described_class.sqs_client.config.region,
+              access_key: described_class.sqs_client.config.credentials.access_key_id }
+      VCR.use_cassette('sqs_send_message', erb: erb) { example.run }
     end
 
-    it { is_expected.to be_a(Seahorse::Client::Response) }
+    it { is_expected.to be_a Seahorse::Client::Response }
 
     context 'when name is string' do
       let(:name) { 'zazo:test' }
-      it { is_expected.to be_a(Seahorse::Client::Response) }
+
+      it { is_expected.to be_a Seahorse::Client::Response }
     end
   end
 
@@ -32,21 +29,18 @@ RSpec.describe Zazo::Tools::EventDispatcher do
     let(:triggered_at) { DateTime.now.utc }
     subject { described_class.build_message(name, params) }
 
-    specify do
+    it do
       Timecop.freeze(triggered_at) do
-        is_expected.to eq(name: %w(zazo test),
-                          triggered_by: 'zazo:api',
-                          triggered_at: DateTime.now.utc)
+        is_expected.to eq name: %w(zazo test), triggered_by: 'zazo:api', triggered_at: DateTime.now.utc
       end
     end
 
     context 'when name is string' do
       let(:name) { 'zazo:test' }
-      specify do
+
+      it do
         Timecop.freeze(triggered_at) do
-          is_expected.to eq(name: %w(zazo test),
-                            triggered_by: 'zazo:api',
-                            triggered_at: DateTime.now.utc)
+          is_expected.to eq name: %w(zazo test), triggered_by: 'zazo:api', triggered_at: DateTime.now.utc
         end
       end
     end
@@ -54,27 +48,29 @@ RSpec.describe Zazo::Tools::EventDispatcher do
 
   describe '.disable_send_message!', event_dispatcher: true do
     subject { described_class.disable_send_message! }
-    specify do
+
+    it do
       expect { subject }.to change { described_class.send_message_enabled? }.from(true).to(false)
     end
   end
 
   describe '.enable_send_message!', event_dispatcher: false do
     subject { described_class.enable_send_message! }
-    specify do
+
+    it do
       expect { subject }.to change { described_class.send_message_enabled? }.from(false).to(true)
     end
   end
 
   describe '.with_state' do
     context 'true' do
-      specify 'send_message_enabled should be truthy' do
+      it 'send_message_enabled should be truthy' do
         described_class.with_state(true) { expect(described_class.send_message_enabled?).to be_truthy }
       end
     end
 
     context 'false' do
-      specify 'send_message_enabled should be falsey' do
+      it 'send_message_enabled should be falsey' do
         described_class.with_state(false) { expect(described_class.send_message_enabled?).to be_falsey }
       end
     end
